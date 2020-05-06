a99ee898d1e6a1f1e3e3d22b29f0f48381f213cf
Saito Naoya <saito3110.naoya@gmail.com>
2020-05-06 09:33:37 +0000

    add error message by email

--- cron/src/logger.py
+++ cron/src/logger.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
+import time
+import smtplib
+import traceback
+from email.mime.text import MIMEText
 from logging import Formatter, handlers, StreamHandler, getLogger, DEBUG, INFO
 
 
 class Logger:
     def __init__(self, name=__name__):
+        self.name = name
         self.logger = getLogger(name)
         self.logger.setLevel(INFO)
         formatter = Formatter(
@@ -39,6 +44,23 @@ class Logger:
 
     def error(self, msg):
         self.logger.error(msg)
+        self.__exc_notification(msg)
 
     def critical(self, msg):
         self.logger.critical(msg)
+        self.__exc_notification(msg)
+
+    def __exc_notification(self, msg):
+        message = str(time.asctime()) + " " + str(os.getpid()) + "\n" + msg + "\n\n" + traceback.format_exc()
+        self.__mail(message)
+
+    def __mail(self, msg):
+        mail = MIMEText(msg, "plain")
+        mail["Subject"] = "["+self.name+"] Error notification"
+        mail["To"] = os.environ["SMTP_TO_ADDRESS"]
+        mail["From"] = os.environ["SMTP_FROM_ADDRESS"]
+
+        server = smtplib.SMTP(os.environ["SMTP_SERVER"], os.environ["SMTP_PORT"])
+        server.send_message(mail)
+        server.quit()
+
